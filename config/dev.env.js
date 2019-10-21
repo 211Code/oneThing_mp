8feaf0d19af54b17443669249d4c9949047b1e79
ede <m18060960803@163.com>
2019-10-22 00:14:24 +0800

    “端口号”

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,3 +4,10 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"'
 })
+// dev.env.js开发环境地址
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  API_BASE_URL: '"http://127.0.0.1:8100/api"'
+})
+
+
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,8 @@
 module.exports = {
   NODE_ENV: '"production"'
 }
+// prod.env.js生产环境地址
+module.exports = {
+  NODE_ENV: '"production"',
+  API_BASE_URL: '"https://www.my-domain.com/api"'
+}
